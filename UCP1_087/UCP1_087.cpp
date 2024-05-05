b9b74586c4569f398b7df6043c8df3d5e2efa2ad
ASUS <ASUS@AZIZDWIMUKTI>
2024-05-05 19:53:45 +0700

    Add project files.

---
+++ UCP1_087/UCP1_087.cpp
@@ -0,0 +1,43 @@
+// 1. 3 deklarasi variable yaitu titik, titik dua, dan koma
+// 2. contoh implementasi conditional statement adalah dalam membuat website untuk menentukan kelulusan mahasiswa
+// 3. contoh implementasi struct adalah mengelompokan jenis gender saat sign in sebuah aplikasi
+// 4. contoh implementasi dari prosedur dan fungsi adalah step menginput data dari seseorang yang akan login suatu aplikasi
+// 5. contoh implementasi dari looping adalah dalam membuat website jadwal keberangkatan kereta
+// 6. 
+#include <iostream>
+using namespace std;
+
+int main() {
+	int nRerata;
+	int nMatematika;
+	string Status;
+	string StatusAsroni;
+	string StatusLia;
+	string StatusJoko;
+
+	cout << "masukan nRerata";
+	cin >> nRerata;
+
+	cout << "masukan nilai matematika";
+	cin >> nMatematika;
+
+	nRerata = 70;
+	nMatematika = 80;
+
+	if (nRerata > 70 || nMatematika > 80) {
+		Status = "Selamat anda lulus";
+	}
+	else {
+		Status = "Maaf, Anda tidak lulus.";
+
+	}
+			(StatusAsroni);
+			Status = "Selamat anda lulus";
+			(StatusLia);
+			Status = "Selamat anda lulus";
+			(StatusJoko);
+			Status = "Maaf, anda ridak lulus";
+
+			return 0;
+
+	
